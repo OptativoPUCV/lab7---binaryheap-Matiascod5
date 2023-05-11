e55b4237f657357f36d6a7343a21dfd1fe86a79a
Replit user <>
2023-05-11 16:59:29 +0000

    new try

--- heap.c
+++ heap.c
@@ -31,8 +31,8 @@ void heap_push(Heap* pq, void* data, int priority){
   if ( pq->size == pq->capac){
     pq->heapArray = realloc( pq->heapArray, sizeof( heapElem) * 2);
   }
-  pq->heapArray[size]->data = data;
-  pq->heapArray[size]->priority = priority;
+  pq->heapArray[pq->size]->data = data;
+  pq->heapArray[pq->size]->priority = priority;
   
 }
 
